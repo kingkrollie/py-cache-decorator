b954f9e89de6b5b99dded2b3f03fe35c5e127fc4
Vadym Vynokur <king.krollie@gmail.com>
2025-10-17 18:51:01 +0300

    add chache, pass all tests

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    used = {}
+
+    def inner(*args) -> Any:
+        if args in used:
+            print("Getting from cache")
+            return used[*args]
+
+        else:
+            print("Calculating new result")
+            used[*args] = func(*args)
+            return used[*args]
+    return inner
